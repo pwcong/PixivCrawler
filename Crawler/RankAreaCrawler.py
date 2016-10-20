194fb025cf60f6d494d486bcff16747f3408b5b6
Pwcong <pwcong@foxmail.com>
2016-10-20 15:00:22 +0800

    实现排行榜大图爬取功能。

---
+++ Crawler/RankAreaCrawler.py
@@ -0,0 +1,111 @@
+# !/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+import PixivLoginer
+from bs4 import BeautifulSoup
+from enum import Enum
+import time
+import os
+
+
+pixiv_ranking_area_url = 'http://www.pixiv.net/ranking_area.php'
+query_type = 'detail'
+
+
+class AreaNo(Enum):
+    beihaidao = 0
+    guandong = 1
+    zhongbu = 2
+    jinji = 3
+    zhongguo = 4
+    jiuzhou = 5
+    guoji = 6
+
+
+class RankAreaItem(object):
+    def __init__(self, illust_id, author, date, browse, score, linkUrl, thumbnailUrl, originalUrl1, originalUrl2):
+        self.illust_id = illust_id
+        self.author = author
+        self.date = date
+        self.browse = browse
+        self.score = score
+        self.linkUrl = linkUrl
+        self.thumbnailUrl = thumbnailUrl
+        self.originalUrl1 = originalUrl1
+        self.originalUrl2 = originalUrl2
+
+
+def analysis(html):
+    items = []
+    rootSoup = BeautifulSoup(html, 'lxml')
+    selector = rootSoup.select('#wrapper > div.layout-body > div > section > div')
+    for child in selector:
+        linkUrl = child.select('div.work_wrapper > a')[0]['href']
+        illust_id = linkUrl.split('=')[-1]
+        thumbnailUrl = child.select('div.work_wrapper > a > div > img')[0]['data-src']
+
+        author = child.select('div.data > a > span')[0].string
+        browse = child.select('div.data > dl.slash-separated > dd')[0].string
+        score = child.select('div.data > dl.slash-separated > dd')[1].string
+        date = child.select('div.data > dl')[1].dd.string
+
+        originalUrl1 = thumbnailUrl.replace('c/150x150/img-master', 'img-original')
+        originalUrl2 = thumbnailUrl.replace('c/150x150/img-master', 'c/1200x1200/img-master')
+
+        items.append(RankAreaItem(illust_id, author, date, browse, score, linkUrl,
+                                  thumbnailUrl, originalUrl1, originalUrl2))
+
+    return items
+
+
+def download_illustration(op, no, picDir):
+
+    print("正在下载中……")
+
+    visit = pixiv_ranking_area_url + '?type=' + query_type + '&no=' + str(no)
+
+    op_visit = op.open(visit)
+    html = PixivLoginer.ungzip(op_visit.read()).decode()
+    op_visit.close()
+
+    items = analysis(html)
+
+    print(items)
+
+    for item in items:
+
+        try:
+            op_img1 = op.open(item.originalUrl1)
+            if op_img1.status == 200:
+                with open(os.path.join(picDir, item.originalUrl1.split('/')[-1]), 'wb') as o:
+                    o.write(op_img1.read())
+            op_img1.close()
+        except:
+            try:
+                op_img2 = op.open(item.originalUrl2)
+                if op_img2.status == 200:
+                    with open(os.path.join(picDir, item.originalUrl2.split('/')[-1]), 'wb') as o:
+                        o.write(op_img2.read())
+                op_img2.close()
+            except:
+                pass
+
+        time.sleep(1)
+
+    print("下载完成！")
+
+
+if __name__ == '__main__':
+
+    userid = input("请输入用户名：")
+    password = input("请输入密码：")
+    saveDir = input("请输入插图保存文件夹路径：")
+
+    areaNo = input("请选择爬取插图排行榜类型（0：北海道 | 1：关东 | 2：中部 | 3：近畿 | 4：中国/四国 | 5：九州/冲绳 | 6：国际）：")
+
+    if int(areaNo) < 0 or int(areaNo) > 6:
+        raise Exception("排行榜类型值超出范围")
+
+    opener = PixivLoginer.login(userid, password)
+
+    download_illustration(opener, areaNo, saveDir)
--- PixivLoginer.py
+++ PixivLoginer.py
@@ -1,14 +1,15 @@
+# !/usr/bin/env python3
+# -*- coding: utf8 -*-
+
 import urllib.request
 import http.cookiejar
 import urllib.parse
 import re
 import gzip
-
+import os.path
 
 pixiv_url_login = "https://accounts.pixiv.net/login"
 pixiv_url_login_post = 'https://accounts.pixiv.net/api/login'
-
-pixiv_url_login_test = 'http://i4.pixiv.net/img-original/img/2016/10/12/19/26/44/59432931_p0.jpg'
 
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -18,51 +19,53 @@ headers = {
     'Host': 'accounts.pixiv.net',
     'Referer': 'http://www.pixiv.net/',
     'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/50.0.2661.102 Safari/537.36'
 }
 
 
-def getopener(headers):
+def getopener(header):
     cj = http.cookiejar.CookieJar()
     cp = urllib.request.HTTPCookieProcessor(cj)
-    opener = urllib.request.build_opener(cp)
+    op = urllib.request.build_opener(cp)
     h = []
-    for key, value in headers.items():
+    for key, value in header.items():
         elem = (key, value)
         h.append(elem)
-    opener.addheaders = h
-    return opener
+    op.addheaders = h
+    return op
 
 
 def getpostkey(body):
     res = re.search('name="post_key" value="\w*', body)
     if res:
-        return (res.group().split('"')[3])
+        return res.group().split('"')[-1]
     else:
         return None
+
 
 def ungzip(data):
     try:
         data = gzip.decompress(data)
-    except:
-        pass
+    except Exception as e:
+        print(e)
     return data
 
 
-def login(id,password):
-
-    opener = getopener(headers)
+def login(uid, pwd):
+    op = getopener(headers)
 
     # 访问登陆界面，获取登陆所需的post_key
-    op_key = opener.open(pixiv_url_login)
+    op_key = op.open(pixiv_url_login)
+
     data = op_key.read()
     op_key.close()
     data = ungzip(data).decode()
 
     # 初始化登陆所需提交的数据
     pixiv_key = getpostkey(data)
-    pixiv_id = id
-    pixiv_password = password
+    pixiv_id = uid
+    pixiv_password = pwd
     pixiv_source = 'accounts'
 
     post_data = {
@@ -74,34 +77,30 @@ def login(id,password):
     post_data = urllib.parse.urlencode(post_data).encode('utf-8')
 
     # 提交登录数据
-    op_login = opener.open(pixiv_url_login_post, post_data)
+    op_login = op.open(pixiv_url_login_post, post_data)
     op_login.close()
 
     # 返回带cookie管理的opener
-    return opener
-
+    return op
 
 
 if __name__ == '__main__':
 
-    id = input('用户名：')
-    password = input('密  码：')
+    pixiv_url_login_test = 'http://i4.pixiv.net/img-original/img/2016/10/12/19/26/44/59432931_p0.jpg'
+
+    userid = input("请输入用户名：")
+    password = input("请输入密码：")
+    path = input("请输入测试图片保存文件夹路径：")
 
     # 登陆Pixiv
-    opener = login(id,password)
+    opener = login(userid, password)
 
     # 下载Pixiv大图测试是否登录成功
     with opener.open(pixiv_url_login_test) as i:
         if i.status == 200:
             print("登陆成功！")
             print("开始下载测试图片！")
-            with open(r'C:\Users\Pwcong\Desktop\test.jpg', 'wb') as o:
+
+            with open(os.path.join(path, pixiv_url_login_test.split('/')[-1]), 'wb') as o:
                 o.write(i.read())
             print("下载完成！")
-
-
-
-
-
-
-
